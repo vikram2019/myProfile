86d25dc6659241d9f0df7eca61609515b68bba35
unknown <vikramku.mahto2015@gmail.com>
2020-03-29 04:42:52 +0530

    first commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let path = require('path');
 // let skillRoutes = require('./backend/skills/skillsRoute/skillsRoutes');
 // let educationRoutes = require('./backend/education/educationRoute/educationRoutes');
 // let profile = require('./backend/profile/profileRoutes/profileRoutes');
-// let config = require('./config/config');
+let config = require('./config/config');
 // let yearRoutes = require('./backend/miscellaneous/routes/yearsRoutes');
 // let monthRoutes = require('./backend/miscellaneous/routes/monthRoutes');
 // let linkRoutes = require('./backend/miscellaneous/routes/linkRoutes');
@@ -45,13 +45,13 @@ app.get('*', function (req, res, next) {
     res.sendFile(path.join(__dirname));
 })
 
-// app.listen(config.port, (err)=>{
-//     if(err){
-//         console.log('Error in connecting in database');
-//     }else{
-//         console.log('Server connected at port '+config.port);
-//     }
-// });
+app.listen(config.port, (err)=>{
+    if(err){
+        console.log('Error in connecting in database');
+    }else{
+        console.log('Server connected at port '+config.port);
+    }
+});
 
 // mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify : false });
 // mongoose.connection.on('Connected', (err)=>{
