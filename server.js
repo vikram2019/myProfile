3a34e706d138ba0e669cb9424f473c47def6e39a
unknown <vikramku.mahto2015@gmail.com>
2020-03-29 17:51:52 +0530

    changes for application

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-// let nodemon = require('nodemon');
-// let morgan = require('morgan');
-// let cors = require('cors');
+let nodemon = require('nodemon');
+let morgan = require('morgan');
+let cors = require('cors');
 let path = require('path');
-// let mongoose = require('mongoose');
+let mongoose = require('mongoose');
 
-// let skillRoutes = require('./backend/skills/skillsRoute/skillsRoutes');
-// let educationRoutes = require('./backend/education/educationRoute/educationRoutes');
-// let profile = require('./backend/profile/profileRoutes/profileRoutes');
+let skillRoutes = require('./backend/skills/skillsRoute/skillsRoutes');
+let educationRoutes = require('./backend/education/educationRoute/educationRoutes');
+let profile = require('./backend/profile/profileRoutes/profileRoutes');
 let config = require('./config/config');
-// let yearRoutes = require('./backend/miscellaneous/routes/yearsRoutes');
-// let monthRoutes = require('./backend/miscellaneous/routes/monthRoutes');
-// let linkRoutes = require('./backend/miscellaneous/routes/linkRoutes');
-// let courseRoutes = require('./backend/miscellaneous/routes/courseRoutes');
+let yearRoutes = require('./backend/miscellaneous/routes/yearsRoutes');
+let monthRoutes = require('./backend/miscellaneous/routes/monthRoutes');
+let linkRoutes = require('./backend/miscellaneous/routes/linkRoutes');
+let courseRoutes = require('./backend/miscellaneous/routes/courseRoutes');
 
 let app = express();
 
 // app.use(morgan(dev));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
-// app.use(cors());
+app.use(cors());
 //This is the static file for Angular 8
 app.use(express.static(path.join(__dirname, '/dist/vikramProfile')));
 
 
-// app.use('/skills', skillRoutes);
-// app.use('/education', educationRoutes);
-// app.use('/profile', profile);
-// app.use('/year', yearRoutes);
-// app.use('/month', monthRoutes);
-// app.use('/link', linkRoutes);
-// app.use('/course', courseRoutes);
+app.use('/skills', skillRoutes);
+app.use('/education', educationRoutes);
+app.use('/profile', profile);
+app.use('/year', yearRoutes);
+app.use('/month', monthRoutes);
+app.use('/link', linkRoutes);
+app.use('/course', courseRoutes);
 
 
-// app.use((data,req, res, next) => {
-//     const error = new Error(data);
-//     error.status = 404;
-//     next(error);
-// });
+app.use((data,req, res, next) => {
+    const error = new Error(data);
+    error.status = 404;
+    next(error);
+});
 
 //This is the Default Route for Angular 6
 app.get('*', function (req, res, next) {
@@ -53,11 +53,11 @@ app.listen(config.port, (err)=>{
     }
 });
 
-// mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify : false });
-// mongoose.connection.on('Connected', (err)=>{
-//     if(err){
-//         console.log('Error in connecting the database '+err);
-//     }else{
-//         console.log('Connected to database at port 27017');
-//     }
-// });
+mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify : false });
+mongoose.connection.on('Connected', (err)=>{
+    if(err){
+        console.log('Error in connecting the database '+err);
+    }else{
+        console.log('Connected to database at port 27017');
+    }
+});
